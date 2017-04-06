ed338062fd61a0f41d78055a82227d4f24e2244d
j-m-c-p <jhonatancubi@gmail.com>
2017-04-06 18:57:49 -0500

    Create funcion.php

---
+++ funcion.php
@@ -0,0 +1,14 @@
+<?PHP
+	if (function_exists('mysq_fetch_array')) 
+	{
+		//mysqli está instalado
+		echo "Si es una función";
+	}else{
+		echo "No es una función";
+	}
+
+	echo "<br><br>";
+	echo "<br><br>";
+
+	//echo phpinfo();
+?>
