769fd67eeb69b7c289a476d57b4cd9336e7cba38
j-m-c-p <jhonatancubi@gmail.com>
2017-04-06 19:03:42 -0500

    Update detectar_si_existe_mysqli.php
    Este archivo funciona con el index.php

--- detectar_si_existe_mysqli.php
+++ detectar_si_existe_mysqli.php
@@ -14,6 +14,7 @@
 	//Esta otra función se encontró en http://stackoverflow.com/questions/16254413/php-5-installed-also-mysqli-is-installed-but-not-compiling-in-mac-lion
 	
 	echo $funcion;
+
 	echo "<br><br>";
 
 	//echo phpinfo();
